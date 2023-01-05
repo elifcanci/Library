52c4389607e801e3f098e23bfaef54d48cfd010e
Elif CANCI <elifcanci5@gmail.com>
2023-01-05 23:13:58 +0300

    Projeye tema giydirildi

--- Library/Controllers/HomeController.cs
+++ Library/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Library.Controllers
 {
-    [Authorize(Policy = "UserPolicy")]
+    //[Authorize(Policy = "UserPolicy")]
     public class HomeController : Controller
     {
         public IActionResult Index()
